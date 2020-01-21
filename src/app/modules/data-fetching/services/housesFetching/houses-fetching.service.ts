9fe2270094be49de01f88d1882cf147e40b5b8f2
Alex <alex.rasi94@gmail.com>
2020-01-21 23:01:58 +0200

    Implement fetchAllHouses method.

--- src/app/modules/data-fetching/services/housesFetching/houses-fetching.service.ts
+++ src/app/modules/data-fetching/services/housesFetching/houses-fetching.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
+import { HousesResponseDTO } from 'src/app/domain/dtos/HousesResponseDTO';
+
+@Injectable()
 export class HousesFetchingService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  fetchAllHouses(): Observable<HousesResponseDTO> {
+    return this.httpClient.get<HousesResponseDTO>('https://demo.interfacema.de/programming-assessment-1.0/buildings');
+  }
 }
