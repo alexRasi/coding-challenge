b17a2e48be4d5bb88ec1a3d1d49d695f6e1ac55c
Alex <alex.rasi94@gmail.com>
2020-01-22 20:24:47 +0200

    Implement getRouteByCoordinates functionality.

--- src/app/modules/geolocation/services/geolocation-api/geolocation-api.service.ts
+++ src/app/modules/geolocation/services/geolocation-api/geolocation-api.service.ts
@@ -6,11 +6,13 @@ import { Observable } from 'rxjs';
 import { AddressDetailsResponseDTO } from 'src/app/domain/dtos/here-api-dtos/AddressDetailsResponseDTO';
 
 import { environment } from 'src/environments/environment';
+import { RouteResponseDTO } from 'src/app/domain/dtos/here-api-dtos/RouteResponseDTO';
 
 @Injectable()
 export class GeolocationApiService {
 
-  private path = 'https://geocoder.ls.hereapi.com/6.2/geocode.json';
+  private geocodeByAddressURL = 'https://geocoder.ls.hereapi.com/6.2/geocode.json';
+  private routeByCoordinatesURL = 'https://route.ls.hereapi.com/routing/7.2/calculateroute.json';
 
   constructor(private httpClient: HttpClient) { }
 
@@ -28,6 +30,23 @@ export class GeolocationApiService {
       .set('country', country)
       .set('apiKey', environment.apiKey);
 
-    return this.httpClient.get<AddressDetailsResponseDTO>(this.path, {params});
+    return this.httpClient.get<AddressDetailsResponseDTO>(this.geocodeByAddressURL, { params });
+  }
+
+  getRouteByCoordinates(
+    waypoint0: string,
+    waypoint1: string,
+    mode: string,
+    departure: string,
+  ): Observable<RouteResponseDTO> {
+
+    const params = new HttpParams()
+      .set('waypoint0', waypoint0.toString())
+      .set('waypoint1', waypoint1)
+      .set('mode', mode)
+      .set('departure', departure)
+      .set('apiKey', environment.apiKey);
+
+    return this.httpClient.get<RouteResponseDTO>(this.routeByCoordinatesURL, { params });
   }
 }
