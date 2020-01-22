37c96c93ef94fbe9f86288f2f4e5220bd4e94263
Alex <alex.rasi94@gmail.com>
2020-01-22 20:09:11 +0200

    Add HERE api key to the environment.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: 'DrteCs7N1f02IBSSSuSjLMPwd-wvuouVrtVv-RJISlE'
 };
 
 /*
