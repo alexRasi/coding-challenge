5dda521762636130868d3c2f405eb310034aa27c
Alex <alex.rasi94@gmail.com>
2020-01-24 01:49:49 +0200

    Implement room filtering tab

--- src/app/modules/ui/components/content-pages/filtered-houses-by-rooms-page/filtered-houses-by-rooms-page.component.ts
+++ src/app/modules/ui/components/content-pages/filtered-houses-by-rooms-page/filtered-houses-by-rooms-page.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { DataProcessingService } from 'src/app/modules/data-processing/services/data-processing/data-processing.service';
+import { HousesFetchingService } from 'src/app/modules/data-fetching/services/housesFetching/houses-fetching.service';
+import { filter } from 'rxjs/operators';
+import { HousesResponseDTO } from 'src/app/domain/dtos/HousesResponseDTO';
+import { HouseDTO } from 'src/app/domain/dtos/HouseDTO';
 
 @Component({
   selector: 'app-filtered-houses-by-rooms-page',
@@ -8,9 +13,22 @@ import { Component, OnInit } from '@angular/core';
 export class FilteredHousesByRoomsPageComponent implements OnInit {
   rooms = 5;
 
-  constructor() { }
+  housesResponse: HousesResponseDTO;
+  modifiedHouses: HouseDTO[] = [];
+
+  constructor(
+    private dataProcessingService: DataProcessingService,
+    private housesFetchingService: HousesFetchingService) { }
 
   ngOnInit() {
+  }
+
+  filterAndSort() {
+    this.housesFetchingService.fetchAllHouses().subscribe((houseResponse: HousesResponseDTO) => {
+      this.housesResponse = houseResponse;
+      this.modifiedHouses = this.dataProcessingService.filterHousesWithMoreThanRooms(houseResponse.houses, this.rooms);
+      this.modifiedHouses = this.dataProcessingService.sortHousesByRoomsDescending(this.modifiedHouses);
+    });
   }
 
 }
