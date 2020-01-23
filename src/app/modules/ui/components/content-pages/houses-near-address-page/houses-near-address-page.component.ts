39cca588e2ccb41cf96e03a62c88ca517b0c9ac5
Alex <alex.rasi94@gmail.com>
2020-01-23 19:40:49 +0200

    Add searchbar in Houses Near Address tab

--- src/app/modules/ui/components/content-pages/houses-near-address-page/houses-near-address-page.component.ts
+++ src/app/modules/ui/components/content-pages/houses-near-address-page/houses-near-address-page.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./houses-near-address-page.component.css']
 })
 export class HousesNearAddressPageComponent implements OnInit {
+  inputStreet = 'Eberswalder Straße';
+  inputNumber = 55;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  findAndSortHousesNearAddress() {
+    
   }
 
 }
