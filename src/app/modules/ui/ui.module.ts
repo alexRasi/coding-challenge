7f50ff5afe8e2d170d8f57bdf57d94289df6e3b6
Alex <alex.rasi94@gmail.com>
2020-01-23 19:54:41 +0200

    Import bootstrap.

--- src/app/modules/ui/ui.module.ts
+++ src/app/modules/ui/ui.module.ts
@@ -6,13 +6,13 @@ import {MatTabsModule} from '@angular/material/tabs';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatInputModule} from '@angular/material/input';
 import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 import { MainPageComponent } from './components/main-page/main-page.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { HousesNearAddressPageComponent } from './components/content-pages/houses-near-address-page/houses-near-address-page.component';
 import { HousesLackingDataPageComponent } from './components/content-pages/houses-lacking-data-page/houses-lacking-data-page.component';
-import { SearchBarComponent } from './components/content-pages/search-bar/search-bar.component';
 // tslint:disable-next-line: max-line-length
 import { FilteredHousesByRoomsPageComponent } from './components/content-pages/filtered-houses-by-rooms-page/filtered-houses-by-rooms-page.component';
 
@@ -23,8 +23,7 @@ import { FilteredHousesByRoomsPageComponent } from './components/content-pages/f
     ToolbarComponent,
     HousesNearAddressPageComponent,
     FilteredHousesByRoomsPageComponent,
-    HousesLackingDataPageComponent,
-    SearchBarComponent,
+    HousesLackingDataPageComponent
     ],
   imports: [
     CommonModule,
@@ -32,7 +31,8 @@ import { FilteredHousesByRoomsPageComponent } from './components/content-pages/f
     MatInputModule,
     MatToolbarModule,
     MatTabsModule,
-    MatIconModule
+    MatIconModule,
+    MatButtonModule
   ],
   exports: [
     MainPageComponent
