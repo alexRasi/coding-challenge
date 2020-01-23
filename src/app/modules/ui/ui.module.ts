4c8717a92dfa3e38b4d2ea71b9b3d009059b0268
Alex <alex.rasi94@gmail.com>
2020-01-23 17:14:37 +0200

    Create search bar component

--- src/app/modules/ui/ui.module.ts
+++ src/app/modules/ui/ui.module.ts
@@ -1,32 +1,38 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import {MatTabsModule} from '@angular/material/tabs';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
 
 import { MainPageComponent } from './components/main-page/main-page.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
-import { HousesNearAddressComponent } from './components/content-pages/houses-near-address/houses-near-address.component';
 import { HousesNearAddressPageComponent } from './components/content-pages/houses-near-address-page/houses-near-address-page.component';
+import { HousesLackingDataPageComponent } from './components/content-pages/houses-lacking-data-page/houses-lacking-data-page.component';
+import { SearchBarComponent } from './components/content-pages/search-bar/search-bar.component';
 // tslint:disable-next-line: max-line-length
 import { FilteredHousesByRoomsPageComponent } from './components/content-pages/filtered-houses-by-rooms-page/filtered-houses-by-rooms-page.component';
-import { HousesLackingDataPageComponent } from './components/content-pages/houses-lacking-data-page/houses-lacking-data-page.component';
 
 @NgModule({
   declarations: [
     MainPageComponent,
     NavigationComponent,
     ToolbarComponent,
-    HousesNearAddressComponent,
     HousesNearAddressPageComponent,
     FilteredHousesByRoomsPageComponent,
     HousesLackingDataPageComponent,
+    SearchBarComponent,
     ],
   imports: [
     CommonModule,
+    FormsModule,
+    MatInputModule,
     MatToolbarModule,
-    MatTabsModule
+    MatTabsModule,
+    MatIconModule
   ],
   exports: [
     MainPageComponent
