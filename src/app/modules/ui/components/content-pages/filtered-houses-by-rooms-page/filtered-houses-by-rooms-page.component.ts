42dad0f566b8b0e52ace75f06162364cd2caece1
Alex <alex.rasi94@gmail.com>
2020-01-24 01:58:07 +0200

    Bind house with missing data.

--- src/app/modules/ui/components/content-pages/filtered-houses-by-rooms-page/filtered-houses-by-rooms-page.component.ts
+++ src/app/modules/ui/components/content-pages/filtered-houses-by-rooms-page/filtered-houses-by-rooms-page.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { DataProcessingService } from 'src/app/modules/data-processing/services/data-processing/data-processing.service';
 import { HousesFetchingService } from 'src/app/modules/data-fetching/services/housesFetching/houses-fetching.service';
-import { filter } from 'rxjs/operators';
 import { HousesResponseDTO } from 'src/app/domain/dtos/HousesResponseDTO';
 import { HouseDTO } from 'src/app/domain/dtos/HouseDTO';
+import { ProgressSpinerService } from '../../../services/progress-spinner/progress-spiner.service';
 
 @Component({
   selector: 'app-filtered-houses-by-rooms-page',
@@ -17,6 +17,7 @@ export class FilteredHousesByRoomsPageComponent implements OnInit {
   modifiedHouses: HouseDTO[] = [];
 
   constructor(
+    private progressSpinerService: ProgressSpinerService,
     private dataProcessingService: DataProcessingService,
     private housesFetchingService: HousesFetchingService) { }
 
@@ -24,10 +25,12 @@ export class FilteredHousesByRoomsPageComponent implements OnInit {
   }
 
   filterAndSort() {
+    this.progressSpinerService.showSpinner();
     this.housesFetchingService.fetchAllHouses().subscribe((houseResponse: HousesResponseDTO) => {
       this.housesResponse = houseResponse;
       this.modifiedHouses = this.dataProcessingService.filterHousesWithMoreThanRooms(houseResponse.houses, this.rooms);
       this.modifiedHouses = this.dataProcessingService.sortHousesByRoomsDescending(this.modifiedHouses);
+      this.progressSpinerService.hideSpinner();
     });
   }
 
